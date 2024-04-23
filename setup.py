0776ac79ef3b4123e53841bb4ff352c0891dd88d
Vedanth <vedanth.aggarwal@gmail.com>
2024-04-23 13:44:24 +0400

    changes

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
 # Local package in virtual env
+
+from setuptools import find_packages,setup
+
+setup(
+    name='mcqgenerator',
+    version=0.01,
+    author='vedanth aggarwal',
+    author_email='vedanth.aggarwal@gmail.com',
+    install_requires=['openai','langchain','streamlit','python-dotenv','PyPDF2'],
+    packages=find_packages()
+)
